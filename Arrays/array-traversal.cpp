1dd93d512e0ad0086ea82f804faecc86abdd1303
Abdullah <imabd489@gmail.com>
2025-07-18 16:24:38 +0500

    Create array-traversal.cpp

---
+++ Arrays/array-traversal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Topic: Array Traversal Operation
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    cout << "Array elements are:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "Index: " << i << "   Value: " << arr[i] << endl;
+    }
+
+    return 0;
+}
